fea61c7b2d6379a71a4adfac4499f5da419fa1b4
Andrew Simms <simms.andrew@gmail.com>
2014-03-19 21:39:36 -0600

    Triggering led when the an input is read

--- on_off.py
+++ on_off.py
@@ -5,14 +5,21 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 
+
 signal_in = 18
 
+led = 22
+
 io.setup(signal_in, io.IN)
+io.setup(led, io.OUT)
 
 while True:
 	try:
 		print io.input(signal_in)
-
+		if io.input(signal_in):
+			io.output(led, 1)
+		else:
+			io.output(led, 0)
 		time.sleep(1)
 	except KeyboardInterrupt:
 		io.cleanup()
