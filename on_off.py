bc9243e5418867ab3c9c693c71c92df4ccba8441
Andrew Simms <simms.andrew@gmail.com>
2014-03-19 22:16:55 -0600

    Changed the way the off input is detected

--- on_off.py
+++ on_off.py
@@ -5,6 +5,7 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 
+sequence_length = sys.argv[1]
 
 signal_in = 18
 
@@ -13,13 +14,19 @@ led = 22
 io.setup(signal_in, io.IN)
 io.setup(led, io.OUT)
 
+sequence = []
+
 while True:
 	try:
-		print io.input(signal_in)
-		if io.input(signal_in):
+		signal = io.input(signal_in)
+		print signal
+		sequence.append(signal)
+		if 0 in sequence:
 			io.output(led, 1)
 		else:
 			io.output(led, 0)
+		if len(sequence) > sequence_length:
+			sequence.pop(0)
 		time.sleep(1)
 	except KeyboardInterrupt:
 		io.cleanup()
