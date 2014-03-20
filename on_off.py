b10370f32f6f2eef3e690e6a18d60362d25e4f57
Andrew Simms <simms.andrew@gmail.com>
2014-03-19 20:52:23 -0600

    Removed variable output.

--- on_off.py
+++ on_off.py
@@ -5,13 +5,9 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 
-signal_out = 16
 signal_in = 18
 
-io.setup(signal_out, io.OUT)
 io.setup(signal_in, io.IN)
-
-io.output(signal_out, 1)
 
 while True:
 	try:
