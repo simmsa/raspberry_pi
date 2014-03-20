9608bd8646b8afceddb27d94a057e87c90a457d3
Andrew Simms <simms.andrew@gmail.com>
2014-03-20 13:55:38 -0600

    Removed while loop from on off function.

--- on_off.py
+++ on_off.py
@@ -8,22 +8,43 @@ io.setmode(io.BOARD)
 signal_in = 18
 
 
-def on_off(signal_in, sequence_length):
-	io.setup(signal_in, io.IN)
-	sequence = []
-	while True:
-		try:
-			signal = io.input(signal_in)
-			sequence.append(signal)
-			if 0 not in sequence:
-				yield True, "Switch is on."
-			else:
-				yield False, "Switch is off."
-			if len(sequence) > sequence_length:
-				sequence.pop(0)
-			time.sleep(0.01)
-		except KeyboardInterrupt:
-			io.cleanup()
-			sys.exit()
+# def on_off(signal_in, sequence_length):
+# 	io.setup(signal_in, io.IN)
+# 	sequence = []
+# 	while True:
+# 		try:
+# 			signal = io.input(signal_in)
+# 			sequence.append(signal)
+# 			if 0 not in sequence:
+# 				yield True, "Switch is on."
+# 			else:
+# 				yield False, "Switch is off."
+# 			if len(sequence) > sequence_length:
+# 				sequence.pop(0)
+# 			time.sleep(0.01)
+# 		except KeyboardInterrupt:
+# 			io.cleanup()
+# 			sys.exit()
 
-print on_off(signal_in, 5).next()
+sequence = []
+
+def check_on_off(signal_in, sequence_length):
+	io.setup(signal_in, io.IN)
+	signal = io.input(signal_in)
+	sequence.prepend(signal)
+	if len(sequence) > sequence_length:
+		sequence.pop()
+
+	if 0 not in sequence:
+		return "Switch is on!"
+	else:
+		return "Switch is off!"
+
+while True:
+	try:
+		print check_on_off(signal_in, 5)
+		time.sleep(0.01)
+	except KeyboardInterrupt:
+		io.cleanup()
+		sys.exit(1)
+
