144d9a1d6fb629bec364b46be26beb0375362ab9
Andrew Simms <simms.andrew@gmail.com>
2014-03-20 18:29:36 -0600

    Adding threading for the temp sensor reading

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import glob
 import time
 import sys
+import thread
 
 import RPi.GPIO as io
 
@@ -24,12 +25,9 @@ def read_temp_raw():
 	return lines
 
 def read_temp():
-	start = time.time()
 	f = open(device_file, "r")
-	open_time = time.time() - start
 	lines = f.readlines()
 	f.close()
-	raw_time = time.time() - start
 
 	equals_pos = lines[1].find("t=")
 
@@ -37,9 +35,20 @@ def read_temp():
 		temp_string = lines[1][equals_pos + 2:]
 		temp_c = float(temp_string) / 1000.0
 		temp_f = temp_c * 9.0 / 5.0 + 32
-		return temp_f, open_time, raw_time, time.time() - start
+		return temp_f
 	else:
-		return False, time.time() - start
+		return False
+
+def continuous_read_temp(delay):
+	while True:
+		try:
+			temp = read_temp()
+
+			if temp:
+				yield temp
+				time.sleep(delay)
+		except KeyboardInterrupt:
+			pass
 
 ######################## Switch On/Off
 
@@ -65,20 +74,23 @@ previous_temp = None
 
 while True:
 	try:
-		if check_on_off(switch_in, 5):
-			print "Checking Temp..."
-			temp_reading = read_temp()
-			if temp_reading:
-				print temp_reading
-				previous_temp = temp_reading
-			elif previous_temp:
-				print previous_temp
-			else:
-				print "No temp readings yet."
-		else:
-			print "Switch Off"
+	# 	if check_on_off(switch_in, 5):
+	# 		print "Checking Temp..."
+	# 		temp_reading = read_temp()
+	# 		if temp_reading:
+	# 			print temp_reading
+	# 			previous_temp = temp_reading
+	# 		elif previous_temp:
+	# 			print previous_temp
+	# 		else:
+	# 			print "No temp readings yet."
+	# 	else:
+	# 		print "Switch Off"
 
-		time.sleep(0.001)
+	# 	time.sleep(0.001)
+
+		thread.start_new_thread(continuous_read_temp, (5))
+
 	except KeyboardInterrupt:
 		io.cleanup()
 		sys.exit(1)
