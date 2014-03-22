38d72dba071af0607aa6b4f25440d9ad5d842e0b
Andrew Simms <simms.andrew@gmail.com>
2014-03-22 17:05:18 -0600

    Added exceptions to both twitter calls.

--- tweet.py
+++ tweet.py
@@ -13,17 +13,22 @@ api = twitter.Api(
 
 # api.PostUpdate("This is the first tweet")
 def temp_request(current_temp):
-	latest_status = api.GetHomeTimeline(count=1)
-	latest_status_text = latest_status[0].text.lower()
-	latest_status_id = latest_status[0].id
+	try:
+		latest_status = api.GetHomeTimeline(count=1)
+		latest_status_text = latest_status[0].text.lower()
+		latest_status_id = latest_status[0].id
 
-	if "what" in latest_status_text and "temperature" in latest_status_text:
-		api.DestroyStatus(latest_status_id)
-		print "Getting and tweeting temp!"
-		api.PostUpdate("The current temperature is %d deg. %s" % (current_temp, time.ctime()))
+		if "what" in latest_status_text and "temperature" in latest_status_text:
+			api.DestroyStatus(latest_status_id)
+			print "Getting and tweeting temp!"
+			api.PostUpdate("The current temperature is %d deg. %s" % (current_temp, time.ctime()))
+	except:
+		print "There is a problem with connecting to twitter, please check your internet connection."
+		pass
 
 def tweet(tweet):
 	try:
 		api.PostUpdate("%s %s" % (tweet, time.ctime()))
 	except:
+		print "There is a problem connecting to twitter, could not check the temperature at this time?"
 		pass
