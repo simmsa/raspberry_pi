c64975c3a02d12cb310451716164a29c4e74df44
Andrew Simms <simms.andrew@gmail.com>
2014-04-15 10:02:24 -0600

    Initial progress on adding plotting function.

---
+++ plot.py
@@ -0,0 +1,33 @@
+import matplotlib
+import numpy as np
+import time
+
+# Saves images, rather than opening up a window
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+font = {"family": "Proxima Nova", "weight": "light", "size": 18}
+
+matplotlib.rc('font', **font)
+matplotlib.rc('axes', edgecolor="w")
+matplotlib.rc('xtick.major', size=0)
+matplotlib.rc('xtick.minor', size=0)
+matplotlib.rc('ytick.major', size=0)
+matplotlib.rc('ytick.minor', size=0)
+
+data = [1, 2, 4, 5, 4, 3]
+
+plt.plot(data,'c', linewidth=2.5)
+plt.ylim(min(data) - 1, max(data) + 1)
+
+data_len = np.arange(len(data))
+
+ydata_copy = data[:]
+ydata_copy[0] = ""
+
+date = time.strftime("%B %d %I:%M %p")
+
+plt.xticks(data_len, ("", date, "", "", date, ""))
+plt.yticks(data, ("", 2, 3, 4, 5))
+
+plt.savefig("font-test.png")
